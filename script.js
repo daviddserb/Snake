8a3a31fafac069496b942279c56f2a26b7e0d576
Serb David <83017887+daviddserb@users.noreply.github.com>
2023-12-31 16:37:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,113 +20,113 @@ let gameInterval; // Declare a variable to store the interval identifier
 
 function generateBoard() {
     // Create the squares
-	for (let row = 0; row < GRID_WIDTH; ++row) {
-		for (let col = 0; col < GRID_HEIGHT; ++col) {
-			let square = document.createElement("div");
-		    GRID.appendChild(square); // Add the square to the grid
+    for (let row = 0; row < GRID_WIDTH; ++row) {
+        for (let col = 0; col < GRID_HEIGHT; ++col) {
+            let square = document.createElement("div");
+            GRID.appendChild(square); // Add the square to the grid
 
-		    // Snake initial position
-		    if (row == 7 && col >= 5 && col <= 7) square.classList.add("snake");
+            // Snake initial position
+            if (row == 7 && col >= 5 && col <= 7) square.classList.add("snake");
 
-		    /// First apple initial position
-		    if (row == 7 && col == 16) square.classList.add("apple");
+            /// First apple initial position
+            if (row == 7 && col == 16) square.classList.add("apple");
 
-		    squaresMatrix[row][col] = square;
-		}
-	}
+            squaresMatrix[row][col] = square;
+        }
+    }
 }
 
 function setSnakeDirection(e) {
-	if (isGamePaused) return;
+    if (isGamePaused) return;
 
-	let arrow = e.code;
+    let arrow = e.code;
 
-	if (snakeDirection === "standing" && (arrow === "ArrowUp" || arrow === "ArrowRight" || arrow === "ArrowDown")) {
-		gameInterval = setInterval(snakeMove, SNAKE_SPEED); // Start the game
-		PAUSE_BUTTON.style.display = "inline-block"; // Show the Pause button
-	}
+    if (snakeDirection === "standing" && (arrow === "ArrowUp" || arrow === "ArrowRight" || arrow === "ArrowDown")) {
+        gameInterval = setInterval(snakeMove, SNAKE_SPEED); // Start the game
+        PAUSE_BUTTON.style.display = "inline-block"; // Show the Pause button
+    }
 
-	if (arrow == "ArrowLeft" && snakeDirection != "standing" && snakeDirection != "right") {
-		snakeDirection = "left";
-	} else if (arrow == "ArrowUp" && snakeDirection != "down") {
-		snakeDirection = "up";
-	} else if (arrow == "ArrowRight" && snakeDirection != "left") {
-		snakeDirection = "right";
-	} else if (arrow == "ArrowDown" && snakeDirection != "up") {
-		snakeDirection = "down";
-	}
+    if (arrow == "ArrowLeft" && snakeDirection != "standing" && snakeDirection != "right") {
+        snakeDirection = "left";
+    } else if (arrow == "ArrowUp" && snakeDirection != "down") {
+        snakeDirection = "up";
+    } else if (arrow == "ArrowRight" && snakeDirection != "left") {
+        snakeDirection = "right";
+    } else if (arrow == "ArrowDown" && snakeDirection != "up") {
+        snakeDirection = "down";
+    }
 }
 
 // Snake moves means - remove tail and increase head
 function snakeMove() {
-	if (isGameLost == true || didSnakeWin() == true) return;
+    if (isGameLost == true || didSnakeWin() == true) return;
 
-  	let snakeTailLine = snakeArray[0].lineS;
-	let snakeTailCol = snakeArray[0].colS;
+      let snakeTailLine = snakeArray[0].lineS;
+    let snakeTailCol = snakeArray[0].colS;
 
-	// Remove snake tail
-	squaresMatrix[snakeTailLine][snakeTailCol].classList.remove("snake");
-	snakeArray.shift();
+    // Remove snake tail
+    squaresMatrix[snakeTailLine][snakeTailCol].classList.remove("snake");
+    snakeArray.shift();
 
-	let snakeHeadLine = snakeArray[snakeArray.length - 1].lineS;
-	let snakeHeadCol = snakeArray[snakeArray.length - 1].colS;
+    let snakeHeadLine = snakeArray[snakeArray.length - 1].lineS;
+    let snakeHeadCol = snakeArray[snakeArray.length - 1].colS;
 
-	if (snakeDirection == "down") {
-		++snakeHeadLine;
-		// if snake hits bottom border => restart from top
-		if (snakeHeadLine == GRID_WIDTH) snakeHeadLine = 0;
-	} else if (snakeDirection == "up") {
-		--snakeHeadLine;
-		//if snake hits top border => restart from bottom
-		if (snakeHeadLine == -1) snakeHeadLine = GRID_WIDTH - 1;
-	} else if (snakeDirection == "right") {
-		++snakeHeadCol;
-		//if snake hits right border => restart from left
-		if (snakeHeadCol == GRID_HEIGHT) snakeHeadCol = 0;
-	} else {
-		--snakeHeadCol;
-		//if snake hits left border => restart from right
-		if (snakeHeadCol == -1) snakeHeadCol = GRID_HEIGHT - 1;
-	}
+    if (snakeDirection == "down") {
+        ++snakeHeadLine;
+        // if snake hits bottom border => restart from top
+        if (snakeHeadLine == GRID_WIDTH) snakeHeadLine = 0;
+    } else if (snakeDirection == "up") {
+        --snakeHeadLine;
+        //if snake hits top border => restart from bottom
+        if (snakeHeadLine == -1) snakeHeadLine = GRID_WIDTH - 1;
+    } else if (snakeDirection == "right") {
+        ++snakeHeadCol;
+        //if snake hits right border => restart from left
+        if (snakeHeadCol == GRID_HEIGHT) snakeHeadCol = 0;
+    } else {
+        --snakeHeadCol;
+        //if snake hits left border => restart from right
+        if (snakeHeadCol == -1) snakeHeadCol = GRID_HEIGHT - 1;
+    }
 
-	// Increase snake head
-	snakeArray.push({lineS: snakeHeadLine, colS: snakeHeadCol});
-	if (didSnakeHitItself() == false) {
-		squaresMatrix[snakeHeadLine][snakeHeadCol].classList.add("snake");
-		didSnakeEatApple(snakeHeadLine, snakeHeadCol, snakeTailLine, snakeTailCol);
-	}
+    // Increase snake head
+    snakeArray.push({lineS: snakeHeadLine, colS: snakeHeadCol});
+    if (didSnakeHitItself() == false) {
+        squaresMatrix[snakeHeadLine][snakeHeadCol].classList.add("snake");
+        didSnakeEatApple(snakeHeadLine, snakeHeadCol, snakeTailLine, snakeTailCol);
+    }
 }
 
 function didSnakeHitItself() {
     for (let row = 0; row < snakeArray.length; ++row) {
-		for (let col = row + 1; col < snakeArray.length; ++col) {
-			if (snakeArray[row].lineS == snakeArray[col].lineS && snakeArray[row].colS == snakeArray[col].colS) {
+        for (let col = row + 1; col < snakeArray.length; ++col) {
+            if (snakeArray[row].lineS == snakeArray[col].lineS && snakeArray[row].colS == snakeArray[col].colS) {
                 isGameLost = true;
                 gameFinished();
-			}
-		}
-	}
-	return false;
+            }
+        }
+    }
+    return false;
 }
 
 // If Snake eat apple => increase snake tail
 function didSnakeEatApple(snakeHeadLine, snakeHeadCol, snakeTailLine, snakeTailCol) {
-	if (squaresMatrix[snakeHeadLine][snakeHeadCol].classList.contains("snake") && squaresMatrix[snakeHeadLine][snakeHeadCol].classList.contains("apple")) {
-		squaresMatrix[snakeHeadLine][snakeHeadCol].classList.remove("apple")
+    if (squaresMatrix[snakeHeadLine][snakeHeadCol].classList.contains("snake") && squaresMatrix[snakeHeadLine][snakeHeadCol].classList.contains("apple")) {
+        squaresMatrix[snakeHeadLine][snakeHeadCol].classList.remove("apple")
 
-		squaresMatrix[snakeTailLine][snakeTailCol].classList.add("snake");
-		snakeArray.unshift({lineS: snakeTailLine, colS: snakeTailCol});
+        squaresMatrix[snakeTailLine][snakeTailCol].classList.add("snake");
+        snakeArray.unshift({lineS: snakeTailLine, colS: snakeTailCol});
 
-		eatAppleSound()
-		
-		SCORE.innerHTML = ++cntApples;
-		if (didSnakeWin() == false) spawnNewApple();
-	}
+        eatAppleSound()
+        
+        SCORE.innerHTML = ++cntApples;
+        if (didSnakeWin() == false) spawnNewApple();
+    }
 }
 
 function eatAppleSound() {
-	var eatAppleSound = document.getElementById("eatAppleSound");
-	eatAppleSound.play();
+    var eatAppleSound = document.getElementById("eatAppleSound");
+    eatAppleSound.play();
 }
 
 function spawnNewApple() {
@@ -148,12 +148,12 @@ function spawnNewApple() {
 }
 
 function didSnakeWin() {
-	const snakeInitialDimension = 3;
+    const snakeInitialDimension = 3;
 
-	if (snakeArray.length == GRID_WIDTH * GRID_HEIGHT - snakeInitialDimension) {
+    if (snakeArray.length == GRID_WIDTH * GRID_HEIGHT - snakeInitialDimension) {
         gameFinished();
-	}
-	return false;
+    }
+    return false;
 }
 
 function gameFinished() {
@@ -166,7 +166,7 @@ function gameFinished() {
 function togglePause() {	
     isGamePaused = !isGamePaused;
 
-	PAUSE_BUTTON.innerHTML = isGamePaused ? "▶" : "||";
+    PAUSE_BUTTON.innerHTML = isGamePaused ? "▶" : "||";
 
     PAUSE_BUTTON.classList.toggle("pause", isGamePaused);
     PAUSE_BUTTON.classList.toggle("resume", !isGamePaused);
@@ -176,11 +176,11 @@ function togglePause() {
 }
 
 function clearPauseButton() {
-	PAUSE_BUTTON.style.display = "none";
+    PAUSE_BUTTON.style.display = "none";
 }
 
 function printStatusGame() {
-	GAME_STATUS.innerHTML = isGameLost ? "GAME OVER! YOU LOST!" : "CONGRATULATIONS! YOU WON!";
+    GAME_STATUS.innerHTML = isGameLost ? "GAME OVER! YOU LOST!" : "CONGRATULATIONS! YOU WON!";
 }
 
 generateBoard();
